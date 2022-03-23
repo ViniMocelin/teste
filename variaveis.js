0fdeb56d3c995274b4b9d9668c5c01284a458e2b
Vinícius Mocelin <87328327+ViniMocelin@users.noreply.github.com>
2022-03-23 16:20:30 -0300

    novas

---
+++ variaveis.js
@@ -0,0 +1,43 @@
+var x = "Aline";
+var y = 29;
+
+console.log("Nome: " + x)
+console.log("Idade: " + y)
+
+var cliente = "Alcides Garcia";
+var conta = 67768734
+var saldo = 322.95
+
+console.log("Cliente: " + cliente + "\nConta: " + conta + "\nSaldo " + saldo);
+
+var deposito = 50.00
+saldo = saldo + deposito;
+
+console.log("Novo depósito na conta: " + conta + "\nSaldo atualizado: " + saldo);
+
+var num1 = 25
+var num2 = 34
+
+console.log(num1 == num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+
+var nome1 = "Vinícius"
+var nome2 = "vinícius"
+
+console.log(nome1 === nome2);
+console.log(nome1 != nome2);
+console.log(nome1 !== nome2);
+
+var preco = 1900
+
+if (preco <= 1900) {
+    console.log("Eu compro a TV");
+} else{
+    console.log("deixa pra próxima");
+}
+ 
+var curso = 'Resilia Webdev';
+var versao = 'V 2.0';
+var cursoVersao = curso + " " + versao;
+
